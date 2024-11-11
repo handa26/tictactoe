b7afb5e5fb6dcce2b796ae82c81fee0e93650d1c
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-11-11 14:47:49 +0800

    Update game flow logic

--- script.js
+++ script.js
@@ -8,35 +8,75 @@ const p1Name = player1.userName;
 const p2Name = player2.userName;
 
 // State of the board
-const gameBoard = (function() {
+const gameBoard = (function () {
   const board = [];
   for (let i = 0; i < 9; i++) {
-    board.push("");
+    board.push({ mark: "", idx: i });
   }
+
+  const winningPatterns = [
+    // Rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    // Columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    // Diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
   return {
     board,
+    winningPatterns,
   }
 })();
 
 // Flow of the game
-const gameController = (function() {
-  const { board } = gameBoard;
+const gameController = (function () {
+  const { board, winningPatterns } = gameBoard;
   let turn = 1;
 
-  const checkGame = () => {};
+  const checkGame = (playerName, playerMark) => {
+    let result = [];
+    const marks = board.filter((mark) => {
+      if (mark.mark === playerMark) {
+        result.push(mark.idx);
+      }
+    });
+
+    // Compare the mark players to the win pattern
+    for (let i = 0; i < winningPatterns.length; i++) {
+      if (JSON.stringify(result) === JSON.stringify(winningPatterns[i])) {
+        alert(`${playerName} win!!!`);
+        return true;
+      }
+    }
+  };
 
   const playerTurn = (playerName, playerMark) => {
-    let row = prompt(`Choose which row for ${playerName}: `);
-    board.splice(row, 1, playerMark);
+    let row;
+
+    do {
+      row = prompt(`Choose which row for ${playerName}: `);
+    } while (parseInt(row) < 0 || parseInt(row) > 8 || board[parseInt(row)].mark !== "");
+
+    const index = parseInt(row);
+    board.splice(index, 1, { mark: playerMark, idx: index });
   }
 
   const playGame = () => {
     if (turn === 1) {
       playerTurn(p1Name, p1Mark);
+      checkGame(p1Name, p1Mark);
       turn = 2;
     } else if (turn === 2) {
       playerTurn(p2Name, p2Mark);
+      checkGame(p2Name, p2Mark);
       turn = 1;
     }
   }
@@ -48,7 +88,7 @@ const gameController = (function() {
 
 function player(name) {
   const userName = name;
-  
+
   function mark(str) {
     return str;
   }
